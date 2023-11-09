93a87289615b30e9a4ee9e443e6caf571cbbb490
JacksonBowe <jackson.bowe@powertec.com.au>
2023-11-09 16:33:26 +1000

    Update user_controller.py

--- packages/core/controllers/user_controller.py
+++ packages/core/controllers/user_controller.py
@@ -1,4 +1,27 @@
-from ..tables.test import TABLE
+import os
+import boto3
 
+cognito = boto3.client('cognito-idp')
 
-TEST = "AYO"
+class UserController:
+    def __init__(self) -> None:
+        pass
+    
+    def invite_user(self, email: str, customer_id: str) -> None:
+        try: 
+            response = cognito.admin_create_user(
+                UserPoolId=os.environ['APP_USER_POOL_ID'],
+                Username=email,
+                UserAttributes=[
+                    {
+                        'Name': 'email',
+                        'Value': email
+                    },
+                    {
+                        'Name': 'email_verified',
+                        'Value': 'true'
+                    }
+                ]
+            )
+        except cognito.exceptions.UsernameExistsException as e:
+            print(str(e))
